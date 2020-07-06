af74b67ac02fcba2d3f0692fb678953c92aed910
Anton Kharlamov <antonio100500@ya.ru>
2020-07-06 21:05:38 +0300

    ТЗ-3

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const hamburger  = document.querySelector(".hamburger");
+const fullscreen = document.querySelector(".fullscreen");
+const 
+hamburger.addEventListener("click", function() {
+    hamburger.classList.add(".fullscreen");
+}
