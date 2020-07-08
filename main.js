896575cb9583a00417afd5d7a04e77eb7f5bf4c1
Anton Kharlamov <antonio100500@ya.ru>
2020-07-08 22:48:47 +0300

    Внесение правок в код

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const hamburger  = document.querySelector(".hamburger");
 const cross = document.querySelector(".cross");
 const fullscreen = document.querySelector(".fullscreen");
 
-hamburger.addEventListener('click', function(){
+hamburger.addEventListener('click', function(event){
+    event.preventDefault();
     fullscreen.style.display = "block";
 })
