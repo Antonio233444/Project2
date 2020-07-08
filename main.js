b8684cdbdbc687255288629819485baa55f570d8
Anton Kharlamov <antonio100500@ya.ru>
2020-07-08 20:14:40 +0300

    Исправление ошибок в main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const hamburger  = document.querySelector(".hamburger");
 const cross = document.querySelector(".cross");
 const fullscreen = document.querySelector(".fullscreen");
 
-hamburger.addEventListener ('click', function(){
+hamburger.addEventListener('click', function(){
     fullscreen.style.display = "block";
 })
